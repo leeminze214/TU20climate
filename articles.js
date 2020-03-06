64002d1469c831224fd1fe54facf20860c22383f
ronjieo <42659888+ronjieo@users.noreply.github.com>
2020-03-06 17:03:04 -0500

    almost done articles

---
+++ articles.js
@@ -0,0 +1,34 @@
+function button1(){
+  document.getElementById("article1").style.display = "block";
+  document.getElementById("article2").style.display = "block";
+  document.getElementById("article3").style.display = "block";
+  document.getElementById("article4").style.display = "block";
+}
+
+function button2(){
+  document.getElementById("article1").style.display = "block";
+  document.getElementById("article2").style.display = "none";
+  document.getElementById("article3").style.display = "none";
+  document.getElementById("article4").style.display = "none";
+}
+
+function button3(){
+  document.getElementById("article1").style.display = "none";
+  document.getElementById("article2").style.display = "block";
+  document.getElementById("article3").style.display = "none";
+  document.getElementById("article4").style.display = "none";
+}
+
+function button4(){
+  document.getElementById("article1").style.display = "none";
+  document.getElementById("article2").style.display = "none";
+  document.getElementById("article3").style.display = "block";
+  document.getElementById("article4").style.display = "none";
+}
+
+function button5(){
+  document.getElementById("article1").style.display = "none";
+  document.getElementById("article2").style.display = "none";
+  document.getElementById("article3").style.display = "none";
+  document.getElementById("article4").style.display = "block";
+}
