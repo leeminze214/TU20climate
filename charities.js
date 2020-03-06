c25a14fa3b7095158bf47c916c416eed552d9692
ronjieo <42659888+ronjieo@users.noreply.github.com>
2020-03-06 18:57:28 -0500

    so much progress

---
+++ charities.js
@@ -0,0 +1,34 @@
+function button1(){
+  document.getElementById("charity1").style.display = "block";
+  document.getElementById("charity2").style.display = "block";
+  document.getElementById("charity3").style.display = "block";
+  document.getElementById("charity4").style.display = "block";
+}
+
+function button2(){
+  document.getElementById("charity1").style.display = "block";
+  document.getElementById("charity2").style.display = "none";
+  document.getElementById("charity3").style.display = "none";
+  document.getElementById("charity4").style.display = "none";
+}
+
+function button3(){
+  document.getElementById("charity1").style.display = "none";
+  document.getElementById("charity2").style.display = "block";
+  document.getElementById("charity3").style.display = "none";
+  document.getElementById("charity4").style.display = "none";
+}
+
+function button4(){
+  document.getElementById("charity1").style.display = "none";
+  document.getElementById("charity2").style.display = "none";
+  document.getElementById("charity3").style.display = "block";
+  document.getElementById("charity4").style.display = "none";
+}
+
+function button5(){
+  document.getElementById("charity1").style.display = "none";
+  document.getElementById("charity2").style.display = "none";
+  document.getElementById("charity3").style.display = "none";
+  document.getElementById("charity4").style.display = "block";
+}
