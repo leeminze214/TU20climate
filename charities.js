aa3ce5fc7703be7ae58433d712c65bdfa899408f
ronjieo <42659888+ronjieo@users.noreply.github.com>
2020-03-06 22:22:02 -0500

    so close

--- charities.js
+++ charities.js
@@ -1,20 +1,20 @@
 function button1(){
-  document.getElementById("charity1").style.display = "block";
-  document.getElementById("charity2").style.display = "block";
-  document.getElementById("charity3").style.display = "block";
-  document.getElementById("charity4").style.display = "block";
+  document.getElementById("charity1").style.display = "flex";
+  document.getElementById("charity2").style.display = "flex";
+  document.getElementById("charity3").style.display = "flex";
+  document.getElementById("charity4").style.display = "flex";
 }
 
 function button2(){
-  document.getElementById("charity1").style.display = "block";
+  document.getElementById("charity1").style.display = "none";
   document.getElementById("charity2").style.display = "none";
-  document.getElementById("charity3").style.display = "none";
+  document.getElementById("charity3").style.display = "flex";
   document.getElementById("charity4").style.display = "none";
 }
 
 function button3(){
-  document.getElementById("charity1").style.display = "none";
-  document.getElementById("charity2").style.display = "block";
+  document.getElementById("charity1").style.display = "flex";
+  document.getElementById("charity2").style.display = "flex";
   document.getElementById("charity3").style.display = "none";
   document.getElementById("charity4").style.display = "none";
 }
@@ -22,13 +22,6 @@ function button3(){
 function button4(){
   document.getElementById("charity1").style.display = "none";
   document.getElementById("charity2").style.display = "none";
-  document.getElementById("charity3").style.display = "block";
-  document.getElementById("charity4").style.display = "none";
-}
-
-function button5(){
-  document.getElementById("charity1").style.display = "none";
-  document.getElementById("charity2").style.display = "none";
   document.getElementById("charity3").style.display = "none";
-  document.getElementById("charity4").style.display = "block";
+  document.getElementById("charity4").style.display = "flex";
 }
